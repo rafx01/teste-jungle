01d6ef16fcb9b45c9fc2b42323aee921463bcda4
Raphael Furini <raphael.furini@hotmail.com>
2025-10-23 00:24:16 -0300

    feat: adicionado app tasks-service + conexao database

---
+++ apps/tasks-service/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + 'dist/migrations/*{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
